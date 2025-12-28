582c69c6732d17660cb738eb81881b45646680ce
GabrielM555 <gabrielmatasoto2005@gmail.com>
2025-12-28 14:00:36 -0400

    feat (docs\manager.controller.mjs) se añade  el modelo de gestión académica con funciones para obtener cursos y crear nuevos

---
+++ backend/src/modules/academic-manager-III/manager.model.mjs
@@ -0,0 +1,17 @@
+// Importar conexión a la base de datos
+import { db } from '../../../database/connection.mjs'
+
+// Obtener cursos de un estudiante
+export const getCoursesByStudentId = async (studentId) => {
+  
+}
+
+// Obtener detalle de un curso
+export const getCourseById = async (courseId) => {
+  
+}
+
+// Crear un nuevo curso
+export const createCourse = async (courseData) => {
+  
+}
